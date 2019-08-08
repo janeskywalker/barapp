44c65eefa90abb42dc888daa6778e8e0f358bf32
dylanpeet <dylanpeet@gmail.com>
2019-08-08 14:09:15 -0700

    Worked on main.html and related css

--- barapp/views.py
+++ barapp/views.py
@@ -3,4 +3,16 @@ from .models import *
 
 # Create your views here.
 def main(request):
-    return render(request, 'main.html')
+    categories = {
+        'Beer': 'Beer',
+        'Wine': 'Wine',
+        'Whiskey': 'Whiskey',
+        'Gin': 'Gin',
+        'Vodka': 'Vodka',
+        'Rum': 'Rum',
+        'Tequila': 'Tequila',
+        'Liquor': 'Liquor',
+        'Other/Mixers': 'Other/Mixers'
+        }
+        
+    return render(request, 'main.html', {'categories': categories})
