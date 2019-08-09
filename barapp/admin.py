4622c5ac242e38322cb7d938f3a8f58eb0c56d12
Jane Skywalker <janehuizeng@gmail.com>
2019-08-08 21:00:32 -0700

    creating category model, fix field error and adding data via admin

--- barapp/admin.py
+++ barapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Tab, Ingredient, Recipe
+from .models import User, Tab, Ingredient, Recipe, Drink, Category
 
 # Register your models here.
 admin.site.register(Tab)
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
+admin.site.register(Drink)
+admin.site.register(Category)
